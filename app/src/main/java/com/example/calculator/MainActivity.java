5cff21fac1c8ca5ee783357502b42fb5e75d04eb
Christian Hall <christian@Christians-MacBook-Pro.local>
2021-01-05 20:10:01 -0500

    not much time this evening, just added button and textview declarations

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button zero = (Button) findViewById(R.id.ZeroButton);
+        Button one = (Button) findViewById(R.id.OneButton);
+        Button two = (Button) findViewById(R.id.TwoButton);
+        Button three = (Button) findViewById(R.id.ThreeButton);
+        Button four = (Button) findViewById(R.id.FourButton);
+        Button five = (Button) findViewById(R.id.FiveButton);
+        Button six = (Button) findViewById(R.id.SixButton);
+        Button seven = (Button) findViewById(R.id.SevenButton);
+        Button eight = (Button) findViewById(R.id.EightButton);
+        Button nine = (Button) findViewById(R.id.NineButton);
+        Button equals = (Button) findViewById(R.id.EqualsButton);
+        Button divide = (Button) findViewById(R.id.Dividebutton);
+        Button multiply = (Button) findViewById(R.id.MultiplyButton);
+        Button subtract = (Button) findViewById(R.id.SubtractButton);
+        Button plusminus = (Button) findViewById(R.id.PlusMinusButton);
+        Button decimal = (Button) findViewById(R.id.DecimalButton);
+        Button clear = (Button) findViewById(R.id.ClearButton);
+        TextView entry = (TextView) findViewById(R.id.EntryView);
+        TextView progress = (TextView) findViewById(R.id.ProgressView);
+
     }
 }
